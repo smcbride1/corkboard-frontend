f710b901b12f91ea123b22607b5252b16d2a9db8
Stephen McBride <stevemcbride3@gmail.com>
2020-09-13 21:24:16 -0400

    Added login fetch on submit

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,12 +9,40 @@ export default class Login extends Component {
         super();
         this.state = {
             email: "",
-            password: ""
+            password: "",
+            error: false,
+            errorMessage: ""
         }
     }
 
-    handleOnSubmit = (event) => {
+    handleEmailChange = (event) => {
+        this.setState({email: event.target.value})
+    }
 
+    handlePasswordChange = (event) => {
+        this.setState({password: event.target.value})
+    }
+
+    handleOnSubmit = (event) => {
+        let formData = new FormData();
+        formData.append('email', this.state.email);
+        formData.append('password', this.state.password);
+
+        fetch('http://localhost:4000/login', {
+        method: 'POST', // or 'PUT'
+        body: formData,
+        })
+        .then(response => response.json())
+        .then(data => {
+        console.log(data.error)
+        if (data.error) {
+            this.setState({error: true});
+            this.setState({errorMessage: data.message});
+        } else {
+            
+        }
+        });
+        event.preventDefault();
     }
 
     render() {
@@ -26,6 +54,13 @@ export default class Login extends Component {
                 </div>
                 <div className="wrapper">
                     <form onSubmit={this.handleOnSubmit}>
+                        {this.state.error ? 
+                        <>
+                            <h2 className="error-message">{this.state.errorMessage}</h2>
+                            <br/>
+                        </>
+                        :
+                        null}
                         <p>Email</p>
                         <input type="text" onChange={this.handleEmailChange} value={this.state.email}></input>
                         <p>Password</p>
