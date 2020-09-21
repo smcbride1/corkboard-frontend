69b95386b5f811a48b1c9ec1821d37fc3f764d80
Stephen McBride <stevemcbride3@gmail.com>
2020-09-21 09:46:09 -0400

    Fixed registration

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,39 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './Home.css';
 import Button from './Button.js';
+import Submit from './Submit.js';
 import InputText from './InputText.js';
+import * as actions from '../actions.js';
 
-export default class Register extends Component {
+export class Register extends Component {
     constructor() {
         super();
         this.state = {
-            name: "",
-            email: "",
-            password: "",
             error: false,
             errorMessage: ""
         }
     }
 
-    handleNameChange = (event) => {
-        this.setState({name: event.target.value})
-    }
-
-    handleEmailChange = (event) => {
-        this.setState({email: event.target.value})
-    }
-
-    handlePasswordChange = (event) => {
-        this.setState({password: event.target.value})
-    }
-
     handleOnSubmit = (event) => {
+        event.preventDefault();
         let formData = new FormData();
-        formData.append('name', this.state.name);
-        formData.append('email', this.state.email);
-        formData.append('password', this.state.password);
+        formData.append('user[name]', document.getElementById("name-input").value);
+        formData.append('user[email]', document.getElementById("email-input").value);
+        formData.append('user[password]', document.getElementById("password-input").value);
 
-        fetch('http://localhost:4000/login', {
+        fetch('http://localhost:4000/users', {
         method: 'POST', // or 'PUT'
         body: formData,
         })
@@ -44,10 +33,11 @@ export default class Register extends Component {
             this.setState({error: true});
             this.setState({errorMessage: data.message});
         } else {
-            this.props.store.dispatch()
+            this.props.setUser(data);
+            this.props.setLoggedIn(true);
+            window.location.href = "/boards";
         }
         });
-        event.preventDefault();
     }
 
     render() {
@@ -58,18 +48,47 @@ export default class Register extends Component {
                     <br/>
                 </div>
                 <div className="wrapper">
-                    <p>Name</p>
-                    <InputText/>
-                    <p>Email</p>
-                    <InputText/>
-                    <p>Password</p>
-                    <InputText/>
-                    <br/>
-                    <br/>
-                    <Button text="Login" path="/login" onClickEvent={this.handleOnClick}></Button>
-                    <Button type="link" text="Register" path="/register"></Button>
+                <form onSubmit={this.handleOnSubmit}>
+                        {this.state.error ? 
+                        <>
+                            <h2 className="error-message">{this.state.errorMessage}</h2>
+                            <br/>
+                        </>
+                        :
+                        null}
+                        <p>Name</p>
+                        <input id="name-input" type="text"></input>
+                        <p>Email</p>
+                        <input id="email-input" type="text"></input>
+                        <p>Password</p>
+                        <input id="password-input" type="password"></input>
+                        <br/>
+                        <br/>
+                        <Submit value="Register"></Submit>
+                    </form>
+                    <Button type="link" text="Login" path="/login"></Button>
                 </div>
             </>
         );
     }
 }
+
+const mapStateToProps = (state, ownProps) => {
+    return {
+        name: state.user.name,
+        email: state.user.email,
+        loggedIn: state.user.loggedIn
+    };
+};
+   
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+        setUser: (user) => dispatch(actions.setUser(user)),
+        setLoggedIn: (loggedIn) => dispatch(actions.setLoggedIn(loggedIn))
+    };
+};
+   
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Register);
--- src/components/ZoomOutButton.js
+++ src/components/ZoomOutButton.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './ZoomOutButton.css';
-import AddIcon from '../AddIcon.svg'
+import SubtractIcon from '../SubtractIcon.svg'
 
 export default class ZoomOutButton extends Component {
     render() {
         return (
             <button id="zoom-out-button" onClick={this.props.onClickEvent}>
-                <img src={AddIcon}/>
+                <img src={SubtractIcon}/>
             </button>
         );
     }
